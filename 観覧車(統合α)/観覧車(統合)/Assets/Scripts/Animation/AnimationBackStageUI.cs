e5c7b0a688803242cd586659109c5838c6e36fbc
根岸 渉 <teteruru3690@gmail.com>
2020-05-29 15:33:43 +0900

    UIでステージセレクトを追加

---
+++ 観覧車(統合α)/観覧車(統合)/Assets/Scripts/Animation/AnimationBackStageUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationBackStageUI : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    string animatorParameters;
+
+    [SerializeField,Header("ステージセレクトシーンの名前")]
+    string sceneName;
+
+    private void Start()
+    {
+        sceneName = "StageSelect";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator == null)
+        {
+            return;
+        }
+    }
+
+
+    //ステージセレクトに戻らない
+    public void BackCansel()
+    {
+        animator.SetBool(animatorParameters, false);
+    }
+
+    //ステージセレクトに戻る
+    public void BackExecution()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
